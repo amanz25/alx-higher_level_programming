e173b57d87c3255cf8c2e3abad7e71b38b6647d8
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-03-28 12:04:10 -0700

    class Square that defines a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""""Define a class Square with init"""
+
+
+class Square:
+    """initalization/constructor while instance of clss"""
+
+    def __init__(self, size=0):
+        """check the type and set the values of class intance variable"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """square area"""
+        return self.__size ** 2
