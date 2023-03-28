53e94d02f5c15647537163a0b02e69b4fefc2003
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-03-28 12:28:44 -0700

    update the setter method

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,12 +19,12 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, val):
+    def size(self, value):
         """setter func"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         else:
-            if val < 0:
+            if value < 0:
                 raise ValueError("size must be >= 0")
             else:
-                self.__size = val
+                self.__size = value
