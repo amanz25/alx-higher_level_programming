1c15410bcac7ba75526dfb24ac888350686ecc24
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-04-10 12:35:37 -0700

    string representation added for square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class inheritance"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class """
+    def __init__(self, size):
+        """ initialization"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """area method"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """string representation"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
