e3ae041c15cda76874100d568afe4bf901c78ad9
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-04-10 13:10:46 -0700

    update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,14 +4,11 @@
 
 class MyInt(int):
     """ create MyInt class """
-    def __new__(cls, *args, **kwargs):
-        """ create instance """
-        return super(MyInt, cls).__new__(cls, *args, **kwargs)
 
     def __eq__(self, other):
         """ equal method """
-        return other != int(self)
+        return int(other) != int(self)
 
     def __ne__(self, other):
         """ not equal """
-        retrun other == int(self)
+        retrun int(other) == int(self)
