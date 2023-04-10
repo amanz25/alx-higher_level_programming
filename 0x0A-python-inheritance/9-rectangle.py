bc682a57adb8e492f816b677d4d7849ab21d6949
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-04-10 12:11:31 -0700

    area and string representation added on rectangle class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""create a base class"""
+
+
+class BaseGeometry:
+    """class with method return exception"""
+    def area(self):
+        """area function"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", heighit)
+
+    def area(self):
+        """area method"""
+        return self.__width * self .__heoght
+
+    def __str__(self):
+        """string representation"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
