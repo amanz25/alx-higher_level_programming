4b2e99ed8331931cb97ebabe2eec379aba8aae52
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-04-11 10:17:22 -0700

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """function that writes a string to a text file"""
     with open(filename, "w") as f:
-        f.write(text)
+        return f.write(text)
