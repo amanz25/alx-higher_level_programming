bb4b6a41f6c5654e58c13be0653062146cee0a2f
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-04-11 13:32:32 -0700

    updated

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,12 +4,11 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """update"""
-    with open(filename, 'r+') as f:
-        lines = f.readlines()
-        i = 0
-        for line in lines:
-            if line.find(search_string) is not -1:
-                lines.insert(i + 1, new_string)
-            i += 1
-        f.seek(0)
-        f.write("".join(lines))
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
