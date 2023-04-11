2de5cef94297afe7d26df429b9050ff6cb53d50f
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-04-11 11:14:16 -0700

    returns dictionary

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""returns the dictionary description with simple data structure"""
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        val = __import__('6-load_from_json_file')\
+                .load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        val = []
+    val.extend(sys.argv[1:])
+    __import__('5-save_to_json_file')
+    .save_to_json_file(items, "add_item.json")
