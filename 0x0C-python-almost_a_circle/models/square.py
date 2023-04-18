6f62f6dcb476b43ff38387ce114bd1d6807f6445
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-04-17 22:08:51 -0700

    update on getter and setter method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,17 @@ class Square(Rectangle):
         """define constructor"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """get the size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set the size value to the width and height"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """override str function"""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
