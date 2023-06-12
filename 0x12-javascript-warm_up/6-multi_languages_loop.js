c556605ae4125dda1fc15b9ad9929cedc858d2a3
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-06-12 11:23:46 -0700

    script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (const arg in langs) {
+const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const arg in args) {
   console.log(args[arg]);
 }
