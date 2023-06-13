8b60d1d4c946d72bd1c0cc066718a98535838f2d
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-06-13 10:54:22 -0700

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < args.length; i++) {
-  console.log(args[i]);
+for (const arg of args) {
+  console.log(arg);
 }
